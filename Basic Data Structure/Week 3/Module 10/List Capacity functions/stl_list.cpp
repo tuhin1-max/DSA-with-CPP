23dcfda5b6c45aa62e74cf96d575ddb8f578c080
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-08 08:40:26 +0000

    May 8, 2026, 2:40 PM

--- Basic Data Structure/Week 3/Module 10/List Capacity functions/stl_list.cpp
+++ Basic Data Structure/Week 3/Module 10/List Capacity functions/stl_list.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 int main(){
-    // ues clear function:
+    cout << "ues clear function:" << endl;
     list<int>l(10,5);
     cout << l.size() << endl;
     l.clear();
@@ -16,5 +16,12 @@ int main(){
         cout << val << endl;
     }
 
+    cout << "ues resize function:" << endl;
+    
+    list<int>l2(10,7);
+    l2.resize(2);
+    for(int val : l2){
+        cout << val << endl;
+    }
     return 0;
 }
