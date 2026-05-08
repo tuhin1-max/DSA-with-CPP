ccab180fed1138f42e04207b94394ce2adbbbdf5
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-08 09:39:21 +0000

    May 8, 2026, 3:39 PM

--- Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers_02.cpp
+++ Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers_02.cpp
@@ -3,9 +3,30 @@ using namespace std;
 
 int main(){
     cout << "erase function:" << endl;
-    list<int> l = {10,20,30,40,50,60,70};
+    list<int> l = {10,20,30,40,20,20,70};
     // l.erase(next(l.begin(),3));
     l.erase(next(l.begin(),2),next(l.begin(),5));
+    for(int val : l){
+        cout << val << endl;
+    }
+
+    cout << "replace function:" << endl;
+    replace(l.begin(),l.end(),20,100);
+    for(int val : l){
+        cout << val << endl;
+    }
+    
+    cout << "find function:" << endl;
+    auto it = find(l.begin(),l.end(),100);
+    
+    if(it == l.end()){
+        cout << "Not found" << endl;
+    }
+
+    else{
+        cout << "found" << endl;
+    }
+
     for(int val : l){
         cout << val << endl;
     }
