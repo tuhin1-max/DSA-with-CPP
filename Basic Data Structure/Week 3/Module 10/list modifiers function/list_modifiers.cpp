fb4d5bf0fdb633d2a9fae8ac8e200d41934601ab
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-08 08:48:19 +0000

    May 8, 2026, 2:48 PM

--- Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers.cpp
+++ Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers.cpp
@@ -2,6 +2,12 @@
 using namespace std;
 
 int main(){
-    
+    list<int> l = {10,20,30};
+    list<int> l2;
+    l2 = l;
+    // l2.assign(l.begin(),l.end());
+    for(int val : l2){
+        cout << val << endl;
+    }
     return 0;
 }
