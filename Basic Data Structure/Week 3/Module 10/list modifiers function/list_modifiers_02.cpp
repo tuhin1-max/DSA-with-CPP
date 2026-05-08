17ce5cce484a899b61782889c828fa938302e3a5
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-08 09:32:55 +0000

    May 8, 2026, 3:32 PM

--- Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers_02.cpp
+++ Basic Data Structure/Week 3/Module 10/list modifiers function/list_modifiers_02.cpp
@@ -5,6 +5,7 @@ int main(){
     cout << "erase function:" << endl;
     list<int> l = {10,20,30,40,50,60,70};
     // l.erase(next(l.begin(),3));
+    l.erase(next(l.begin(),2),next(l.begin(),5));
     for(int val : l){
         cout << val << endl;
     }
