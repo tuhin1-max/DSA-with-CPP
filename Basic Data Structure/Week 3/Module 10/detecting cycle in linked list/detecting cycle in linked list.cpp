f26b7521850bcb73c16fe1c58ac55c0b2062cf31
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-09 18:10:21 +0000

    May 10, 2026, 12:10 AM

--- Basic Data Structure/Week 3/Module 10/detecting cycle in linked list/detecting cycle in linked list.cpp
+++ Basic Data Structure/Week 3/Module 10/detecting cycle in linked list/detecting cycle in linked list.cpp
@@ -27,7 +27,7 @@ int main(){
     Node* slow = head;
     Node* fast = head;
     bool flag = false;
-    while (fast != NULL)
+    while (fast != NULL && fast->next != NULL)
     {
         slow = slow->next;
         fast = fast->next->next;
