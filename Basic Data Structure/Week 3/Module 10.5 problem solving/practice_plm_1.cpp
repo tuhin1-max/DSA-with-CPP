e791c1eb9833deac4cddadc8ea03fc3d61132d9c
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-11 15:25:46 +0600

    Refactor code structure for improved readability and maintainability

--- Basic Data Structure/Week 3/Module 10.5 problem solving/practice_plm_1.cpp
+++ Basic Data Structure/Week 3/Module 10.5 problem solving/practice_plm_1.cpp
@@ -9,27 +9,85 @@ class Node{
         Node(int val){
             this->val = val;
             this->next = NULL;
-            this->prev = prev;
+            this->prev = NULL;
         }
 };
 
-void insert_at_tail(Node* head, Node* tail, int val){
-    
+void insert_at_tail(Node* &head,Node* &tail,int val){
+    Node* newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+int count_linked_list(Node* head){
+    Node* temp = head;
+    int count = 0;
+    while(temp != NULL){
+        count++;
+        temp=temp->next;
+    }
+    return count;
+}
+
+bool are_same(Node* &head,Node* &head2){
+    for(Node* i = head,*j = head2;i!=NULL && j!=NULL;i=i->next,j=j->next){
+        if(i->val != j->val){
+            return false;
+        }
+    }
+
+    return true;
 }
 
 int main(){
     Node* head = NULL;
     Node* tail = NULL;
-
-    while (1)
-    {
+    
+    while(true){
         int x;
         cin >> x;
-        if(x == -1){
+        if(x==-1){
             break;
         }
-
+        insert_at_tail(head,tail,x);
     }
+
+    int sz1 = count_linked_list(head);
+
+    Node* head2 = NULL;
+    Node* tail2 = NULL;
     
+    while(true){
+        int x2;
+        cin >> x2;
+        if(x2==-1){
+            break;
+        }
+        insert_at_tail(head2,tail2,x2);
+    }
+
+    int sz2 = count_linked_list(head2);
+
+    if(sz1 != sz2){
+        cout << "NO" << endl;
+    }
+
+    else{
+        if(are_same(head,head2)){
+            cout << "YES" << endl;
+        }
+
+        else{
+            cout << "NO" << endl;
+        }
+    }
+
     return 0;
 }
