f65fd947cb922f58e2b71196a2828d0e2d6d5a69
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-11 15:56:46 +0600

    Add implementation for Practice__plm_2 including linked list operations

---
+++ Basic Data Structure/Week 3/Module 10.5 problem solving/Practice__plm_2.cpp
@@ -0,0 +1,60 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+};
+
+void insert_at_tail(Node* &head,Node* &tail,int val){
+    Node* newnode = new Node(val);
+    if(head == NULL){
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+void reverse_linke_list(Node* &head,Node* &tail){
+    for(Node* i=head,*j=tail;i!=j && i->prev!=j;i=i->next,j=j->prev){
+        swap(i->val,j->val);
+    }
+}
+
+void print(Node* head){
+    Node* tmp = head;
+    while (tmp != NULL)
+    {
+        cout << tmp->val << " ";
+        tmp = tmp->next;
+    }
+}
+
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true){
+        int x;
+        cin >> x;
+        if(x==-1){
+            break;
+        }
+        insert_at_tail(head,tail,x);
+    }
+
+    reverse_linke_list(head,tail);
+    print(head);
+    
+    return 0;
+}
