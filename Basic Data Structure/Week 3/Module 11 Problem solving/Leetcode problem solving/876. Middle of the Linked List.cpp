812d92974a203d3ad4e7766b4b9c5591c7f11dc3
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-13 14:14:38 +0600

    Remove unnecessary includes and local test cases from Middle of the Linked List implementation

--- Basic Data Structure/Week 3/Module 11 Problem solving/Leetcode problem solving/876. Middle of the Linked List.cpp
+++ Basic Data Structure/Week 3/Module 11 Problem solving/Leetcode problem solving/876. Middle of the Linked List.cpp
@@ -1,14 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// ==================== STRUCTURE ====================
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-// ==================== SOLUTION ====================
 class Solution {
 public:
     int count_linked_list(ListNode* head) {
@@ -30,30 +19,3 @@ public:
         return temp;
     }
 };
-
-// ==================== LOCAL TEST ====================
-int main() {
-    // Example 1: [1,2,3,4,5]
-    ListNode* head = new ListNode(1);
-    head->next = new ListNode(2);
-    head->next->next = new ListNode(3);
-    head->next->next->next = new ListNode(4);
-    head->next->next->next->next = new ListNode(5);
-
-    Solution sol;
-    ListNode* mid = sol.middleNode(head);
-    cout << "Middle node: " << mid->val << endl; // Output: 3
-
-    // Example 2: [1,2,3,4,5,6]
-    ListNode* head2 = new ListNode(1);
-    head2->next = new ListNode(2);
-    head2->next->next = new ListNode(3);
-    head2->next->next->next = new ListNode(4);
-    head2->next->next->next->next = new ListNode(5);
-    head2->next->next->next->next->next = new ListNode(6);
-
-    ListNode* mid2 = sol.middleNode(head2);
-    cout << "Middle node: " << mid2->val << endl; // Output: 4
-
-    return 0;
-}
