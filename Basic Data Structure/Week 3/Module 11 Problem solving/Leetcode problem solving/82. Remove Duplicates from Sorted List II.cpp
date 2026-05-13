cb6b81aaf0ed2d0fdb73f0b40724cee40a06f7be
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-13 14:13:13 +0600

    Remove unnecessary includes and local test cases from Remove Duplicates from Sorted List II implementation

--- Basic Data Structure/Week 3/Module 11 Problem solving/Leetcode problem solving/82. Remove Duplicates from Sorted List II.cpp
+++ Basic Data Structure/Week 3/Module 11 Problem solving/Leetcode problem solving/82. Remove Duplicates from Sorted List II.cpp
@@ -1,14 +1,3 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-// ==================== STRUCTURE ====================
-struct ListNode {
-    int val;
-    ListNode* next;
-    ListNode(int x) : val(x), next(NULL) {}
-};
-
-// ==================== SOLUTION ====================
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
@@ -27,36 +16,3 @@ public:
         return head;
     }
 };
-
-// ==================== LOCAL TEST ====================
-int main() {
-    Solution sol;
-
-    // Example 1: [1,1,2] → [1,2]
-    ListNode* head1 = new ListNode(1);
-    head1->next = new ListNode(1);
-    head1->next->next = new ListNode(2);
-
-    ListNode* res1 = sol.deleteDuplicates(head1);
-    while (res1 != NULL) {
-        cout << res1->val << " ";
-        res1 = res1->next;
-    }
-    cout << endl; // Output: 1 2
-
-    // Example 2: [1,1,2,3,3] → [1,2,3]
-    ListNode* head2 = new ListNode(1);
-    head2->next = new ListNode(1);
-    head2->next->next = new ListNode(2);
-    head2->next->next->next = new ListNode(3);
-    head2->next->next->next->next = new ListNode(3);
-
-    ListNode* res2 = sol.deleteDuplicates(head2);
-    while (res2 != NULL) {
-        cout << res2->val << " ";
-        res2 = res2->next;
-    }
-    cout << endl; // Output: 1 2 3
-
-    return 0;
-}
