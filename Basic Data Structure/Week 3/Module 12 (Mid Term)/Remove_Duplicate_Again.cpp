4676be3606e3b5522963664ea9fc261586010032
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-17 10:40:55 +0600

    Add implementations for Palindrome and Remove Duplicate Again problems

---
+++ Basic Data Structure/Week 3/Module 12 (Mid Term)/Remove_Duplicate_Again.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    list <int> l;
+    int x;
+    while(cin >> x && x!=-1){
+        l.push_back(x);
+    }
+
+    l.sort();
+    l.unique();
+   
+    for(int val : l){
+        cout << val << " ";
+    }
+
+    return 0;
+}
