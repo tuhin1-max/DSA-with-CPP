09a96504ca95d989649f3584463de579d74815db
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-20 10:07:57 +0600

    Add implementation of Palindrome check with linked list operations

---
+++ Basic Data Structure/Week 3/Module 12 (Mid Term)/Palindrome.cpp
@@ -0,0 +1,66 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+            this->prev = NULL;
+        }
+};
+
+void insert_at_tail(Node* &head,Node* &tail,int val){
+    Node* newnode = new Node(val);
+
+    if(head==NULL){
+        head = newnode;
+        tail = newnode;
+        return;
+    }
+
+    tail->next = newnode;
+    newnode->prev = tail;
+    tail = newnode;
+}
+
+void palindrome(Node* &head,Node* &tail){
+    bool flag = true;
+    while(head!=tail && tail->next!=head){
+        if(head->val!=tail->val){
+            flag = false;
+            break;
+        }
+
+        head = head->next;
+        tail = tail->prev;
+    }
+
+    if(flag){
+        cout << "YES" << endl;
+    }
+
+    else{
+        cout << "NO" << endl;
+    }
+}
+int main(){
+    Node* head = NULL;
+    Node* tail = NULL;
+
+    while(true){
+        int x;
+        cin >> x;
+        if(x==-1){
+            break;
+        }
+        insert_at_tail(head,tail,x);
+    }
+    
+    palindrome(head,tail);
+
+    return 0;
+}
