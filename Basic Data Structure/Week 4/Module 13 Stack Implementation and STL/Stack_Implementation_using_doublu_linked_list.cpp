4c3d07c46d8b13a1ac2e0728e0b2887002469451
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-21 11:01:35 +0600

    Add implementation of stack using doubly linked list with basic operations

---
+++ Basic Data Structure/Week 4/Module 13 Stack Implementation and STL/Stack_Implementation_using_doublu_linked_list.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node* prev;
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+            this->next = NULL;
+        }
+};
+
+class myStack{
+    public:
+    
+        Node* head = NULL;
+        Node* tail = NULL;
+        int sz = 0;
+
+        void push(int val){   //O(1)
+            sz++;
+            Node* newndoe = new Node(val);
+
+            if(head == NULL){
+                head = newndoe;
+                tail = newndoe;
+                return;
+            }
+
+            tail->next = newndoe;
+            newndoe->prev = tail;
+            tail = newndoe;
+        }
+        void pop(){          //O(1)
+            sz--;
+            Node* deletenode = tail;
+
+            tail = tail->prev;
+            delete deletenode;
+            
+            if(tail == NULL){
+                head = NULL;
+                return;
+            }
+
+            tail->next = NULL;
+        }
+        int top(){           //O(1)
+            return tail->val;
+        }
+        int size(){          //O(1)
+            return sz;
+        }
+        bool empty(){        //O(1)
+            if(head == NULL){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
+};
+
+int main(){
+    myStack st;
+    int n;
+    cin >> n;
+    for(int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+    
+
+    return 0;
+}
