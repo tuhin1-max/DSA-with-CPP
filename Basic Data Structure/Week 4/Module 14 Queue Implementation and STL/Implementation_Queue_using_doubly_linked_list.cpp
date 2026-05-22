07baece0a58a83aab3cf4093914f7442baada68d
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-05-22 14:58:33 +0600

    Implement queue using doubly linked list with basic operations

--- Basic Data Structure/Week 4/Module 14 Queue Implementation and STL/Implementation_Queue_using_doubly_linked_list.cpp
+++ Basic Data Structure/Week 4/Module 14 Queue Implementation and STL/Implementation_Queue_using_doubly_linked_list.cpp
@@ -5,9 +5,11 @@ class Node{
     public:
         int val;
         Node* next;
+        Node* prev;
         Node(int val){
             this->val = val;
             this->next = NULL;
+            this->prev = NULL;
         }
 };
 
