471aab4feec57af2215fcdef023b777a1a9a56ba
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-06-10 00:01:46 +0600

    Refactor stack implementation and add new Same_0r_Not problem solution

---
+++ Basic Data Structure/Week 4/Module 16 Assignment 03/Is_It_Valid.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        stack<string>st;
+        string s;
+        
+        while(cin >> s){
+            st.push(s);
+        }
+
+        int count = 0;
+        int count2 = 0;
+    }
+    return 0;
+}
--- Basic Data Structure/Week 4/Module 16 Assignment 03/Same_or_Not_II.cpp
+++ Basic Data Structure/Week 4/Module 16 Assignment 03/Same_or_Not_II.cpp
@@ -14,48 +14,68 @@ class Mystack{
     }
     
     int top(){
-        return v.front();
+        return v.back();
     }
 
     bool empty(){
-        if(v.size()==0){
-            return true;
-        }
-        else{
-            return false;
-        }
+        return v.empty();
     }
+};
+
+class Node{
+    public:
+        int val;
+        Node* next;
+        Node(int val){
+            this->val = val;
+            this->next = NULL;
+        }
 };
 
 class Myqueue{
     public:
-    vector<int> v;
+    Node* head = NULL;
+    Node* tail = NULL;
     
     void push(int val){
-        v.push_back(val);
+        Node* newnode = new Node(val);
+        if(head == NULL){
+            head = newnode;
+            tail = newnode;
+            return;
+        }
+
+        tail->next = newnode;
+        tail = newnode;
     }
 
     void pop(){
-        v.pop_back();
+        Node* deletenode = head;
+        head = head->next;
+        delete deletenode;
+        if(head == NULL){
+            tail = NULL;
+        }
     }
-    
+
     int front(){
-        return v.front();
+        return head->val;
     }
 
     bool empty(){
-        if(v.size()==0){
+        if(head == NULL){
             return true;
         }
         else{
             return false;
         }
     }
+
 };
 
 int main(){
-    Mystack *st;
-    Myqueue *q;
+    Mystack st;
+    Myqueue q;
     
     int n,m;
     cin >> n >> m;
@@ -67,27 +87,27 @@ int main(){
         for(int i=0;i<n;i++){   
             int val;
             cin >> val;    
-            st->push(val);
+            st.push(val);
         }
 
         for(int i=0;i<m;i++){
             int val2;   
             cin >> val2;    
-            q->push(val2);
+            q.push(val2);
         }
         
         bool temp = true;
-        while(!st->empty()){
-            int st_val = st->top();
-            int q_val = q->front();
+        while(!st.empty()){
+            int st_val = st.top();
+            int q_val = q.front();
 
             if(st_val!=q_val){
                 temp = false;
                 break;
             }
 
-            st->pop();
-            q->pop();
+            st.pop();
+            q.pop();
         }
 
         if(temp){
