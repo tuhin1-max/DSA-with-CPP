cf1f9a04213e10a4692484777d9cff8310e077be
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-06-10 10:46:59 +0600

    Add implementation of Elimination problem solution

---
+++ Basic Data Structure/Week 4/Module 16 Assignment 03/Elimination.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        stack<char> st;
+        string s;
+        cin >> s;
+
+        for(char c : s){
+            if(!st.empty() && st.top()=='0' && c=='1'){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+
+        if(st.empty()){
+            cout << "YES" << endl;
+        }
+        else{
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
