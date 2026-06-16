44c0a849243163c1923de0c8887b49005c48ffe9
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-06-16 20:33:26 +0600

    Add implementation for counting nodes in a binary tree with recursion

---
+++ Basic Data Structure/Week 5 Binary Tree/Module 18/count_tree_nodes_with_recursion.cpp
@@ -0,0 +1,84 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+        int val;
+        Node* left;
+        Node* right;
+        Node(int val){
+            this->val = val;
+            this->left = NULL;
+            this->right = NULL;
+        }
+};
+
+Node* input_tree(){
+    int val;
+    cin >> val;
+    Node* root;
+    if(val == -1){
+        root = NULL;
+    }
+    else{
+        root = new Node(val);
+    }
+
+    queue<Node*> q;
+    if(root){
+        q.push(root);
+    }
+
+    while(!q.empty()){
+        // 1. bar kore ana
+        Node* p = q.front();
+        q.pop();
+
+        // 2. oi node neye kaj korbo
+        int l,r;
+        cin >> l >> r;
+
+        Node *myleft , *myright;
+        if(l==-1){
+            myleft = NULL;
+        }
+        else{
+            myleft = new Node(l);
+        }
+
+        if(r==-1){
+            myright = NULL;
+        }
+        else{
+            myright = new Node(r);
+        }
+        
+        p->left = myleft;
+        p->right = myright;
+
+        // 3. children push korbo
+        if(p->left){
+            q.push(p->left);
+        }
+        if(p->right){
+            q.push(p->right);
+        }
+    }
+
+    return root;
+}
+
+int count_nodes(Node* root){
+    if(root == NULL){
+        return 0;
+    }
+
+    int l = count_nodes(root->left);
+    int r = count_nodes(root->right);
+    return l+r+1;
+}
+int main(){
+    Node* root = input_tree();
+    cout << count_nodes(root);
+    return 0;
+}
