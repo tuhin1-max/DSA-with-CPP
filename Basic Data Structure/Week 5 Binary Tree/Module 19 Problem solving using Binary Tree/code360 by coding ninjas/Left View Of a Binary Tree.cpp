e6d339751226f33b04c6add57b6102f010cfea26
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-06-23 15:07:15 +0600

    Add two way implementation for getting left view of a binary tree

--- Basic Data Structure/Week 5 Binary Tree/Module 19 Problem solving using Binary Tree/code360 by coding ninjas/Left View Of a Binary Tree.cpp
+++ Basic Data Structure/Week 5 Binary Tree/Module 19 Problem solving using Binary Tree/code360 by coding ninjas/Left View Of a Binary Tree.cpp
@@ -34,3 +34,39 @@ vector<int> getLeftView(TreeNode<int> *root)
 
     return v;
 }
+
+// 2nd way
+
+#include <bits/stdc++.h> 
+
+vector<int> getLeftView(TreeNode<int> *root)
+{
+    vector<int> ans;
+    bool fre[3005] = {false};
+    queue<pair<TreeNode<int>*,int>> q;
+    if(root){
+        q.push({root,1});
+    }
+
+    while(!q.empty()){
+        pair<TreeNode<int>*,int> parent = q.front();
+        q.pop();
+
+        TreeNode<int>* node = parent.first;
+        int level = parent.second;
+        
+        if(fre[level]==false){
+            ans.push_back(node->data);
+            fre[level] = true;
+        }
+
+        if(node->left){
+            q.push({node->left,level+1});
+        } 
+        if(node->right){
+            q.push({node->right,level+1});
+        }
+    }
+
+    return ans;
+}
