ff0c1053531f0491d326c161b288440ff645220c
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-06-24 14:24:37 +0600

    Add implementation for calculating the diameter of a binary tree

---
+++ Basic Data Structure/Week 5 Binary Tree/Module 19 Problem solving using Binary Tree/code360 by coding ninjas/Diameter Of Binary Tree.cpp
@@ -0,0 +1,20 @@
+int mx;
+int height_tree(TreeNode<int> *root){
+    if(root==NULL){
+        return 0;
+    }
+    if(root->left==NULL && root->right==NULL){
+        return 1;
+    }
+
+    int l = height_tree(root->left);
+    int r = height_tree(root->right);
+    int d = l+r;
+    mx = max(d,mx);
+    return max(l,r)+1;
+}
+int diameterOfBinaryTree(TreeNode<int> *root){
+	mx = 0;
+    int h=height_tree(root);
+    return mx;
+}
