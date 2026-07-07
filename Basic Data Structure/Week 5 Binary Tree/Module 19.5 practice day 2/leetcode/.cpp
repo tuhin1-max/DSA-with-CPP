ba310a56109096a1739ba608babef29f8df72054
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-07-07 17:45:39 +0600

    Refactor code structure for improved readability and maintainability

---
+++ Basic Data Structure/Week 5 Binary Tree/Module 19.5 practice day 2/leetcode/.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int x;
+    cin >> x;
+
+    x = x/10;
+    x = x/10;
+    x = x/10;
+
+    cout << x << endl;
+    
+    return 0;
+}
--- Basic Data Structure/Week 5 Binary Tree/Module 19.5 practice day 2/leetcode/Univalued Binary Tree.cpp
+++ Basic Data Structure/Week 5 Binary Tree/Module 19.5 practice day 2/leetcode/Univalued Binary Tree.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     bool isUnivalTree(TreeNode* root) {
-        if(!root){
-            return true;
-        }
-
         queue<TreeNode*> q;
         q.push(root);
-        
+        bool res = true;
         while(!q.empty()){
             TreeNode* f = q.front();
             q.pop();
-            
-            if(f->val != root->val){
-                return false;
-            }
-
-            if(f->left){
+    
+            if(f->left != NULL) 
+            {
+                if(f->val != f->left->val){
+                    return false;
+                }
                 q.push(f->left);
             }
-            if(f->right){
+            if(f->right != NULL)
+            {
+                if(f->val != f->right->val){
+                    return false;
+                }
                 q.push(f->right);
             }
+            
+            
         }
-        
+
         return true;
     }
 };
