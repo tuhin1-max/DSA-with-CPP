87516f9ae0fa41e962c25e8f7a3d6e9309cde80d
tuhin1-max <mdtuhinahammed2030@gmail.com>
2026-08-06 23:21:23 +0600

    Add implementations for adjacency list and matrix representations of directed and undirected graphs, along with edge list representation

---
+++ Introduction to Algorithms/Week-1 (Graph Traversal Algorithoms)/Module-01 (Graph Representation)/Adjacency_list_of_undirected_graph.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n,e;
+    cin >> n >> e;
+    int a[n];
+    vector<int> adj_list[n]; // array of vector
+    while(e--){
+        int a,b;
+        cin >> a >> b;
+        adj_list[a].push_back(b);
+    }
+
+    for(int i=0;i<n;i++){
+        cout << i << "-->";
+        for(int x : adj_list[i]){
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
